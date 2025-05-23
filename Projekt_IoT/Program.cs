dbfb1297b8f09166eb38a6fe33a769fda56f465f
Dominika <dominika@gmail.com>
2025-05-23 11:13:19 +0200

    utowrzenie proj consoli i device

--- Projekt_IoT/Program.cs
+++ Projekt_IoT/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Projekt_IoT
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using Microsoft.Azure.Devices.Client;
+using Projekt_IoT_Device;
+
+
+string deviceConnectionString = "<your_connection_string>";
+using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+await deviceClient.OpenAsync();
+var device = new VirtualDevice(deviceClient);
+
+await device.InitializeHandlers();
+await device.UpdateTwinAsync();
+
+Console.WriteLine($"Connection success!");
+
+await device.SendMessages(10, 1000);
+
+Console.WriteLine("Finished! Press Enter to close...");
+Console.ReadLine();
