4e8e50f4e55f3e12a912f59d2a2fb6e9bf253f5d
Przemek <przemekjasiaczyk@gmail.com>
2025-05-26 13:46:40 +0200

    Create IOT Device functions and connecting to IOT HUB

--- Projekt_IoT_Device/Program.cs
+++ Projekt_IoT_Device/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Projekt_IoT_Device
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices;
+using Projekt_IoT_Device;
+
+
+
+string conString = "HostName=projekt-iot.azure-devices.net;DeviceId=Device1;SharedAccessKey=6YVojAtq8v4YHIno38bH0S40gi9qBDhg1my9EYP6fHg=";
+Console.WriteLine("|------------------------------------------|");
+Console.WriteLine("[Agent] Azure Conntecting String Loaded");
+
+using var deviceClient = DeviceClient.CreateFromConnectionString(conString);
+await deviceClient.OpenAsync();
+var device = new MyDevice(deviceClient);
+Console.WriteLine("[Agent] Connection with Device Established");
+await device.InitializerHandlers();
+await device.UpdateTwinAsync();
+Console.WriteLine("[Agent] Start Working...");
+Console.WriteLine("");
+Console.WriteLine("");
+await device.SendMessages(2, 1000);
+var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+while (await periodicTimer.WaitForNextTickAsync())
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    await device.TimerSendingMessages();
 }
